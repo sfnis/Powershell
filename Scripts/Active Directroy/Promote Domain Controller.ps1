d40f27ed9eb1fbdfc5b6810efc91c86c279cd1d7
Jeremy Anderson <44121087+sfnis@users.noreply.github.com>
2018-12-27 13:37:44 -0800

    Creating Powershell Directory

---
+++ Scripts/Active Directroy/Promote Domain Controller.ps1
@@ -0,0 +1,23 @@
+﻿
+#
+# Windows PowerShell script for AD DS Deployment
+#
+# Change Variables as necessary
+
+$domain = "Contoso.com"
+$site = "Default Site"
+Import-Module ADDSDeployment
+Install-ADDSDomainController 
+-ADPrepCredential (Get-Credential) 
+-NoGlobalCatalog:$false 
+-CreateDnsDelegation:$false 
+-Credential (Get-Credential) 
+-CriticalReplicationOnly:$false 
+-DatabasePath "C:\Windows\NTDS" 
+-DomainName $domain 
+-InstallDns:$true 
+-LogPath "C:\Windows\NTDS" 
+-NoRebootOnCompletion:$false 
+-SiteName $site 
+-SysvolPath "C:\Windows\SYSVOL" 
+-Force:$true
